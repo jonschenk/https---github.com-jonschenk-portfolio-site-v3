d447ba4d91a2aa24cf0bcfd8a59abf793b4d5bbf
Jon Schenk <93761862+jonschenk@users.noreply.github.com>
2024-04-05 18:20:02 -0700

    so far so good

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 // author jonschenk
 import dynamic from 'next/dynamic';
+import Hub from './components/Hub/Hub';
 
 const Scene = dynamic(() => import('./components/Scene'), {
   ssr: false
@@ -9,6 +10,7 @@ export default function Home() {
   return (
     <main className='relative h-screen'>
       <Scene/>
+      <Hub/>
     </main>
   );
 }
